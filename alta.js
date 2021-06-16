22fefa8c27c07dc776df2798caf0859cd7474274
M Arafat Al Mubarok <arafat@alterra.id>
2021-06-16 20:19:15 +0700

    init project

---
+++ alta.js
@@ -0,0 +1,7 @@
+function average(arr) {
+  // your code here
+}
+
+console.log(average([5, 5, 5, 5, 5])); // 5
+console.log(average([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 5.5
+console.log(average([])); // 0
